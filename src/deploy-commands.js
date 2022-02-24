9dddc7ad90c4ca8c1ae3633c150f76537d1493d3
Thijs Boom <twboom@hotmail.nl>
2022-02-24 20:35:05 +0100

    Added logs to update message

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -2,22 +2,27 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config/validation.json');
+const { log } = require('./functions/utility.js');
+
+log('bot', 'Deploying commands')
 
 const commands = []; // Array for the commands
 
 // Get the commands
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+log('followup', 'Got the command files')
+
 
 // Add the commands to the array
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     commands.push(command.data.toJSON());
 };
-
+log('followup', 'Added the commands to the command list')
 
 // Deploy the commands
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => log('followup', 'Successfully registered application commands'))
 	.catch(console.error);
